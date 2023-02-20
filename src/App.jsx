d7b20a9382d82a2553d907c3040ec2b93c3d4ef4
kimimberlyyen <work223305@gmail.com>
2023-02-20 21:26:29 +0800

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { NavLink, Link, Route, Routes } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
-import { BookRoutes } from './BookRoutes'
 import './style.css'
 
 function App() {
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Fork IT</NavLink>
-          </li>
-          <li>
-            <NavLink end to="/books">
-              {' '}
-              Books{' '}
-            </NavLink>
-          </li>
-        </ul>
+        <NavLink to="/">Fork IT</NavLink>
       </nav>
+
+      <div className="container">
+        <div className="main">
+          <NavLink end to="/books">
+            <img src="../src/assets/home.png" alt="home_PNG_Idea_to_Travel" />
+          </NavLink>
+        </div>
+      </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/books/*" element={<BookRoutes />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
